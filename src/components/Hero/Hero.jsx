4c29fe6c0f6a26d73fe69ecb3032d9abf08d2c9f
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-07-26 16:59:21 -0300

    Execução exercício 03

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import './Hero.css'
 function Hero() {
+
+    const [local, setLocal] = useState('');
+
+
+
     return (
         <>
-                <h1>HERO</h1>    
+            <div className='hero'>
+            <h3>Bebida rápida, gelada e no precinho? <br /> O Zé entrega tudo.</h3>
+            <form>
+                 <input 
+                type="text" 
+                placeholder='Inserir endereço para ver preço' 
+                value={local} 
+                onChange={(e) => setLocal(e.target.value)} 
+                />
+            </form> 
+            </div>   
         </>
     )
 }
